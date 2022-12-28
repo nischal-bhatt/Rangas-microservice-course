9c09bbb01e605fe876f22ab4eaee7bcd0771aa0a
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-12-28 16:41:16 +0800

    microservices set up basically done

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,39 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+
+	@Autowired
+	private CurrencyExchangeRepository repository;
+	
+	@Autowired
+	private Environment environment;
+	
+	 @GetMapping("/currency-exchange/from/{from}/to/{to}")
+	 public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to)
+	 {
+		// CurrencyExchange ex= new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+	 
+		 CurrencyExchange ex = this.repository.findByFromAndTo(from, to);
+		 
+		 if (ex==null)
+		 {
+			 throw new RuntimeException("unable to find data for " + from + "to " + to);
+		 }
+		 
+		 String port = environment.getProperty("local.server.port");
+		 ex.setEnvironment(port);
+		 
+		 return ex;
+		 
+		 
+	 }
+}

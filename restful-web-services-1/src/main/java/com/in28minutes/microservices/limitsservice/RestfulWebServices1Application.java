077f98d64bc95fc918b3cee34b60be406564434f
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-12-28 12:43:42 +0800

    small limits project

---
+++ restful-web-services-1/src/main/java/com/in28minutes/microservices/limitsservice/RestfulWebServices1Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServices1Application.class, args);
+	}
+
+}

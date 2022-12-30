e9f077287a91ffaf5452c59ca9be19f4c738f275
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-12-31 06:54:20 +0800

    docker first notes

---
+++ Notes-Project/src/Notes.java
@@ -0,0 +1,18 @@
+
+public class Notes {
+
+	public static void main(String[] args) {
+		System.out.println(""
+				+ " small microservices - communicate with each other to achieve functionality \n"
+				+ " some ms can be written in java.. others in python eg.. all in the same project \n"
+				+ " but.... deployments become complex! \n"
+				+ " how to get common way to deploy multiple ms ?\n"
+				+ " that is where containers come into the pic\n"
+				+ " create docker image for each microservice \n"
+				+ " install docker - docker desktop.. this is the docker engine\n"
+				+ " docker desktop is not automatically started up \n"
+				+ " MAC + linux\n"
+				+ " ");
+	}
+
+}
